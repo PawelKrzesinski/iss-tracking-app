52a01de724df672020cae1c315c6a8e3dbf600db
Pawel <pawel.krzesinski1989@gmail.com>
2020-07-04 15:08:47 +0100

    Updates

---
+++ precache-manifest.90cea1a1d04a8f5f5350ab125064e30e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3b315ce77cca24d917695584c688c7e",
+    "url": "/PawelKrzesinski/iss-tracking-app/index.html"
+  },
+  {
+    "revision": "d4c023a5beccafa3b390",
+    "url": "/PawelKrzesinski/iss-tracking-app/static/css/2.7c35e809.chunk.css"
+  },
+  {
+    "revision": "8d66160f3c995021f964",
+    "url": "/PawelKrzesinski/iss-tracking-app/static/css/main.79c792cc.chunk.css"
+  },
+  {
+    "revision": "d4c023a5beccafa3b390",
+    "url": "/PawelKrzesinski/iss-tracking-app/static/js/2.a050217e.chunk.js"
+  },
+  {
+    "revision": "da75538c52411804769b514107a743de",
+    "url": "/PawelKrzesinski/iss-tracking-app/static/js/2.a050217e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d66160f3c995021f964",
+    "url": "/PawelKrzesinski/iss-tracking-app/static/js/main.6e60144e.chunk.js"
+  },
+  {
+    "revision": "9c805687f055023805ce",
+    "url": "/PawelKrzesinski/iss-tracking-app/static/js/runtime-main.2f87ce45.js"
+  }
+]);
